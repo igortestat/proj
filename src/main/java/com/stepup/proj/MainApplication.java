c3ffecde68084e8d3f4fd3d645f3802460d7d020
igortestat <igorsiv1405@gmail.com>
2025-08-07 15:46:13 +0300

    доработка логики проекта2400

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,6 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("11111111333311");
+        System.out.println("111111991133333311");
     }
 }
