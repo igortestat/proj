b638ae2c49a17dbbcfb3919566b8207a93523227
igortestat <igorsiv1405@gmail.com>
2025-08-07 16:49:59 +0300

    измениили логику main do

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -1,13 +1,16 @@
 package com.stepup.proj;
 
+import javax.swing.*;
+
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("---");
         System.out.println("Hello World");
         System.out.println("111");
     }
     public static void doSomething(){
+        System.out.println("0");
         System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+        System.out.println("1");
     }
     }
